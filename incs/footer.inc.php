db778bc2fe00a256348b402d6b284c1b4cb0f729
Wilfried <wilfried.djopa@gmail.com>
2020-03-22 21:44:36 +0100

    Initial Commit

---
+++ incs/footer.inc.php
@@ -0,0 +1,8 @@
+<div class="row  d-flex justify-content-center mt-4 mb-4">
+    <div class="col-10 ">
+        <hr>
+        <div class="text-center">
+            Makers against COVID-19.
+        </div>
+    </div>
+</div>
---
+++ pages/accueil.php
@@ -0,0 +1,27 @@
+
+<div class="container">
+    <div class="row d-flex justify-content-center m-5">
+        <img src="images/virus.svg" alt="" width="80" class="img-fluid">
+    </div>
+    <div class="row d-flex justify-content-center ">
+        <div class="col-11">
+            <div class="card " style="box-shadow: 4px 4px 4px #888;">
+                <div class="card-body">
+                    <h4 class="text-center">Note d'information</h4>
+                    <p class="text-center pt-4">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam libero laboriosam corporis fuga quis sit, quia fugit illo, consequatur pariatur placeat laborum laudantium eligendi consectetur, minima totam cupiditate vero? Dicta?
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row  d-flex justify-content-center mt-4 mb-4">
+        <div class="col-11">
+            <a href="?page=diagnostique" class="btn btn-rounded text-uppercase p-3 mb-2 mt-2 w-100 btn-primary">Commencer le diagnostique</a>
+            <a href="?page=questions" class="btn btn-rounded text-uppercase  p-3 mb-2 mt-2 w-100 btn-danger">Poser les questions</a>
+            <a href="?page=examens" class="btn btn-rounded  text-uppercase p-3 mb-2  mt-2 w-100 btn-success">Liste des centres d'examens</a>
+        </div>
+    </div>
+
+</div>
