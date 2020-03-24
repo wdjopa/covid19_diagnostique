7d6e528a90da0aa32ce567901c541c3e676e6e30
Wilfried <wilfried.djopa@gmail.com>
2020-03-24 19:55:36 +0100

    Update color theme

--- incs/footer.inc.php
+++ incs/footer.inc.php
@@ -1,8 +1,11 @@
-<div class="row  d-flex justify-content-center mt-4 mb-4">
-    <div class="col-10 ">
-        <hr>
-        <div class="text-center">
-            Makers against COVID-19.
+<div class="row">
+    <div class="container  d-flex justify-content-center mt-4 mb-4">
+
+        <div class="col-10 ">
+            <hr>
+            <div class="text-center">
+                Makers against COVID-19.
+            </div>
         </div>
     </div>
 </div>
